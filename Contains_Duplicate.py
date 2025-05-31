b59eaa3b697d7e050d20a7b10c68c481fe01e445
AhnafZ778 <141830611+AhnafZ778@users.noreply.github.com>
2025-05-31 06:57:24 +0600

    Create Contains_Duplicate.py

---
+++ Contains_Duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        s1 = set(nums)
+        if len(s1) == len(nums):
+            return False
+        else:
+            return True
