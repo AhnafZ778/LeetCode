d034e117bb66fa08f7488b3cf7a9de70b7df1866
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-03 12:35:47 +0600

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -16,3 +16,22 @@ class Solution:
                           res.append(l1)      
       return res
 ## I pray for your eyes
+
+## This is the optimal solution O(n^2) here we basically use two pointers while keeping a pointer at the start and iterating slowly from the start, while doing so we use two pointers one
+## from the immediate next index of current one and the other from the last, everything else is pretty standard 
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i,a in enumerate(nums):
+            l,r = i + 1, len(nums)-1
+            while l < r:
+                if a + nums[l] + nums[r] > 0:
+                    r -= 1
+                elif a + nums[l] + nums[r] < 0:
+                    l += 1
+                else:
+                    if [a,nums[l],nums[r]] not in res:
+                        res.append([a, nums[l], nums[r]])
+                    l += 1
+        return res
