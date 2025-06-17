7a8b30df007ebf44a8c90f3127f35eb36391f30a
Ahnaf Bin Zakaria <141830611+AhnafZ778@users.noreply.github.com>
2025-06-17 08:21:20 +0600

    Update LCA.py

--- LCA.py
+++ LCA.py
@@ -4,6 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+## It's better to use iterative method of solving here 
+## other than recursive because it's much simpler since
+## in BST we only need to go right or left depending on 
+## if the root node val is greater or lesser than both 
+## provided node and if it's greater or lesser than only one
+## in that case that is the required Ancestor Node
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
